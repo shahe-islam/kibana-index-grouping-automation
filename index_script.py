88244ecd1b8028ac242527ba3c5e4d05f7c45ee2
Shahe Islam <shahe.islam@nationwide.co.uk>
2021-01-25 17:00:31 +0000

    refactored elif block to use dictionary

--- index_script.py
+++ index_script.py
@@ -19,21 +19,14 @@ LOG_SIZE_MB = 'mb'
 ##### JOURNEY #####
 ###################
 
-# journeys = {
-#     EDB = ['edb', 'containerlogs-edb'],
-#     HOME = ['home', 'containerlogs-home'],
-#     OB = ['ob', 'obcompete'],
-#     MYNW = ['mynw', 'mynationwide'],
-#     RAAS = ['raas'],
-#     NDAP = ['ndap', 'ops', 'containerlogs', 'telegraf', 'beat', 'tgw', 'watcher', 'prod', 'monitoring'],
-# }
-
-EDB = ['edb', 'containerlogs-edb']
-HOME = ['home', 'containerlogs-home']
-OB = ['ob', 'obcompete']
-MYNW = ['mynw', 'mynationwide']
-RAAS = ['raas']
-NDAP = ['ndap', 'ops', 'containerlogs', 'telegraf', 'beat', 'tgw', 'watcher', 'prod', 'monitoring']
+journeys = {
+    'EDB':['edb', 'containerlogs-edb'],
+    'HOME':['home', 'containerlogs-home'],
+    'OB':['ob', 'obcompete'],
+    'MYNW':['mynw', 'mynationwide'],
+    'RAAS':['raas'],
+    'NDAP':['ndap', 'ops', 'containerlogs', 'telegraf', 'beat', 'tgw', 'watcher', 'prod', 'monitoring'],
+}
 
 for i in range(2, ws.max_row + 1):
 
@@ -51,21 +44,10 @@ for i in range(2, ws.max_row + 1):
 
     index = ws.cell(row=i, column=3).value
 
-    if any(x in index for x in EDB):
-        journey = 'EDB'
-    elif any(x in index for x in HOME):
-        journey = 'HOME'
-    elif any(x in index for x in OB):
-        journey = 'OB'
-    elif any(x in index for x in MYNW):
-        journey = 'MYNW'
-    elif any(x in index for x in RAAS):
-        journey = 'RAAS'
-    elif any(x in index for x in NDAP):
-        journey = 'NDAP'
-    else:
-        journey = 'default'
-    
+    for journey, options in journeys.items():
+        if any(x in index for x in options): break
+    else: journey = 'default'
+
     ws.cell(row=i, column=12).value = journey
 
 #example save_path /Users/Shahe.Islam/developer/ndap-journey/ndap-journey-test2.xlsx
