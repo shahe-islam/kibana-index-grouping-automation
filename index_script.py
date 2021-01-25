8d8cd6b3e70a5e1e9ccd4f312253992836870296
Shahe Islam <shahe.islam@nationwide.co.uk>
2021-01-25 17:23:57 +0000

    refactored sizing into own function

--- index_script.py
+++ index_script.py
@@ -12,16 +12,17 @@ ws = wb['Sheet 1']
 ##### COLUMNS #####
 ###################
 
-INDEX_COLUMN = 3
-SIZE_COLUMN = 11
-HUB_COLUMN = 12
+INDEX = 3
+ORIGINAL_SIZE = 9
+SCALED_SIZE = 11
+HUB = 12
 
 ###################
 ##### SIZES #######
 ###################
 
-LOG_SIZE_GB = 'gb'
-LOG_SIZE_MB = 'mb'
+INDEX_SIZE_GB = 'gb'
+INDEX_SIZE_MB = 'mb'
 
 ###################
 ##### JOURNEY #####
@@ -36,29 +37,32 @@ journeys = {
     'NDAP':['ndap', 'ops', 'containerlogs', 'telegraf', 'beat', 'tgw', 'watcher', 'prod', 'monitoring'],
 }
 
-for i in range(2, ws.max_row + 1):
-
-    size = ws.cell(row=i, column=9).value
-
-    if LOG_SIZE_GB in size:
-        size = size.strip(LOG_SIZE_GB)
-        size = str(float(size)*10000)
-    elif LOG_SIZE_MB in size:
-        size = size.strip(LOG_SIZE_MB)
+def index_size_scaler(size):
+    if INDEX_SIZE_GB in size:
+        size = size.strip(INDEX_SIZE_GB)
+        size = str(float(size)*1024)
+    elif INDEX_SIZE_MB in size:
+        size = size.strip(INDEX_SIZE_MB)
     else:
         size = 0
+    return size
 
-    ws.cell(row=i, column=SIZE_COLUMN).value = size
+for i in range(2, ws.max_row + 1):
 
-    index = ws.cell(row=i, column=INDEX_COLUMN).value
+    size = ws.cell(row=i, column=ORIGINAL_SIZE).value
+    scaled_size = index_size_scaler(size)
+
+    ws.cell(row=i, column=SCALED_SIZE).value = scaled_size
+
+    index = ws.cell(row=i, column=INDEX).value
 
     for journey, options in journeys.items():
         if any(x in index for x in options): break
     else: journey = 'default'
 
-    ws.cell(row=i, column=HUB_COLUMN).value = journey
+    ws.cell(row=i, column=HUB).value = journey
 
-#example save_path /Users/Shahe.Islam/developer/ndap-journey/ndap-journey-test2.xlsx
+#example save_path /Users/Shahe.Islam/developer/ndap-journey/ndap-journey-test.xlsx
 
 save_path = input("Input the file save path: ")
 wb.save(save_path)
